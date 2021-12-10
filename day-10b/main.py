5e251f66dd5d1532feb1f760a10ef0fc5c73fcc5
Davide Costantini <d.costantini6@campus.unimib.it>
2021-12-10 12:21:42 +0100

    Day 10

--- day-10b/main.py
+++ day-10b/main.py
@@ -2,36 +2,41 @@ file_name = "input.txt"
 with open(file_name) as input_file:
     input_file_rows = input_file.readlines()
 
-score_dict = {')': 3, ']': 57, '}': 1197, '>': 25137}
-error_score = 0
+score_dict = {'(': 1, '[': 2, '{': 3, '<': 4}
+score_list = []
 for row in input_file_rows:
     char_list = ""
+    counter = 0
     for char in row.strip():
         if char in {'(', '[', '{', '<'}:
             char_list += char
         else:
             if char_list[-1] == '(':
                 if char != ')':
-                    error_score += score_dict[char]
                     break
                 else:
                     char_list = char_list[:-1]
             elif char_list[-1] == '[':
                 if char != ']':
-                    error_score += score_dict[char]
                     break
                 else:
                     char_list = char_list[:-1]
             elif char_list[-1] == '{':
                 if char != '}':
-                    error_score += score_dict[char]
                     break
                 else:
                     char_list = char_list[:-1]
             elif char_list[-1] == '<':
                 if char != '>':
-                    error_score += score_dict[char]
                     break
                 else:
                     char_list = char_list[:-1]
-print(error_score)
+        counter += 1
+    if counter == len(row.strip()):
+        score = 0
+        for char in char_list[::-1]:
+            score = score * 5 + score_dict[char]
+        score_list.append(score)
+
+score_list.sort()
+print(score_list[int(len(score_list) / 2)])
