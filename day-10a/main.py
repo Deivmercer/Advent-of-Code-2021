8311c86b670ed8705b2717a04d02dbf0e70b529f
Davide Costantini <d.costantini6@campus.unimib.it>
2021-12-10 11:46:34 +0100

    Day 10a

---
+++ day-10a/main.py
@@ -0,0 +1,38 @@
+file_name = "input.txt"
+with open(file_name) as input_file:
+    input_file_rows = input_file.readlines()
+
+score_dict = {')': 3, ']': 57, '}': 1197, '>': 25137}
+error_score = 0
+char_list = ""
+for row in input_file_rows:
+    char_list = ""
+    for char in row.strip():
+        if char in {'(', '[', '{', '<'}:
+            char_list += char
+        else:
+            if char_list[-1] == '(':
+                if char != ')':
+                    error_score += score_dict[char]
+                    break
+                else:
+                    char_list = char_list[:-1]
+            elif char_list[-1] == '[':
+                if char != ']':
+                    error_score += score_dict[char]
+                    break
+                else:
+                    char_list = char_list[:-1]
+            elif char_list[-1] == '{':
+                if char != '}':
+                    error_score += score_dict[char]
+                    break
+                else:
+                    char_list = char_list[:-1]
+            elif char_list[-1] == '<':
+                if char != '>':
+                    error_score += score_dict[char]
+                    break
+                else:
+                    char_list = char_list[:-1]
+print(error_score)
