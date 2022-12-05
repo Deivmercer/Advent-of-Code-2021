147d7900e18308b04cc86bdf14ad01d7d7cafc40
Davide Costantini <davemercerdave@gmail.com>
2022-12-05 16:34:00 +0100

    Day 05a

---
+++ 2022/day-05a/day-05a.cpp
@@ -0,0 +1,68 @@
+#include <fstream>
+#include <string>
+#include <cassert>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+const std::string file_name = "input.txt";
+
+int main()
+{
+	std::ifstream input(file_name);
+
+	assert(input.is_open() && !input.eof());
+
+	std::vector<std::vector<char>> stacks;
+	std::string line;
+
+	std::getline(input, line);
+	for (size_t i = 0; i <= line.size() / 4; ++i)
+	{
+		std::vector<char> stack;
+		stacks.push_back(stack);
+	}
+
+	do
+	{
+		if (line.empty())
+			continue;
+
+		for (size_t i = 0; i < line.size(); i += 4)
+		{
+			char crate = line.substr(i, 3)[1];
+			if (crate != ' ')
+				stacks[i / 4].insert(stacks[i / 4].begin(), crate);
+		}
+
+		std::getline(input, line);
+	} while (line.substr(0, 3) != " 1 ");
+
+	while (!input.eof())
+	{
+		std::getline(input, line);
+
+		if (line.empty())
+			continue;
+
+		std::stringstream stream(line);
+		size_t q, from, to;
+
+		stream.ignore(5);
+		stream >> q;
+		stream.ignore(5);
+		stream >> from;
+		stream.ignore(3);
+		stream >> to;
+
+		for (size_t i = 0; i < q; ++i)
+		{
+			char crate = stacks[from - 1].back();
+			stacks[from - 1].pop_back();
+			stacks[to - 1].push_back(crate);
+		}
+	}
+
+	for (std::vector<char> stack : stacks)
+		std::cout << stack.back();
+}
